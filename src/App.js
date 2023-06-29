af695e78b85f1cf68ca874e2c7146aea29d904da
Timothy White <bigtball321@yahoo.com>
2023-06-28 20:23:25 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
+const Login = () => {
+  return (<h2>Login</h2>)
+}
+const FriendsList = () => {
+  return (<h2>FriendsList</h2>)
+}
+const AddFriend = () => {
+  return (<h2>AddFriend</h2>)
+}
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Route exact path="/">
+      <Login/>  
+      </Route>
+      <Route exact path="/login">
+        <Redirect to="/"/>
+      </Route>
+      <Route exact path="/friends">
+        <FriendsList/>  
+      </Route>
+      <Route exact path="/friends/add">
+      <AddFriend/>  
+      </Route>
+     
     </div>
+    </Router>
   );
 }
 
